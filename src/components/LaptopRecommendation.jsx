4661985f3e7c04c9067026b22c133b190fabe19c
unknown <abir045@gmail.com>
2023-08-23 13:43:57 +0600

    added pagination to filtered results

--- src/components/LaptopRecommendation.jsx
+++ src/components/LaptopRecommendation.jsx
@@ -61,16 +61,16 @@ const LaptopRecommendation = () => {
          const meetsRAM = ram === '' || laptop.desc.some(descLine => descLine.toLowerCase().includes(ram)); // Check if any description line contains the RAM
          const meetsStorage = storage === '' || laptop.desc.some(descLine => descLine.toLowerCase().includes(storage)); // Check if any description line contains the storage
             
+            
            
             return  meetsDisplay && meetsRAM && meetsStorage;
 
-        //    return meetsBudget
+        
         })
 
         setLaptops(filteredLaptops) 
-        // setDisplay('')
-        // setRam('')
-        // setStorage('')
+        setTotalPages(Math.ceil((filteredLaptops.length / itemsPerPage)))
+        
     }
 
     
@@ -79,7 +79,7 @@ const LaptopRecommendation = () => {
   return (
     <div className='flex flex-col justify-center my-[5%] items-center'>
         
-        <h1 className='font-bold text-center mb-10 p-5'>Laptop Recommendation for Asus from Ryans & Startech </h1>
+    <h1 className='font-bold text-center mb-10 p-5'>Laptop Recommendation for Asus from Ryans & Startech </h1>
         
         <q className='text-sm my-3 italic'>Please input display, ram and storage , follow the placeholder pattern </q>
         
@@ -93,7 +93,7 @@ const LaptopRecommendation = () => {
         <input type='text' value={storage.toLowerCase()} name='storage' onChange={handleChange} placeholder='1tb....' />
 
         {/* <button type='submit'>Recommend</button> */}
-        <button type="sumbit" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Recommend</button>
+        <button type="submit" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Recommend</button>
 
 
         </form>
@@ -107,6 +107,8 @@ const LaptopRecommendation = () => {
                 <Laptop key={item.url} item={item}  />
             ) )
          }
+
+        
 
          
 
